94f018f1e18995fffba3034653b584c1baeff50b
frankdulko <79063207+frankdulko@users.noreply.github.com>
2022-02-14 23:47:03 -0500

    Initial Commit

---
+++ ChatDemo/ChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoApp.swift
+//  ChatDemo
+//
+//  Created by Frank Dulko on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
